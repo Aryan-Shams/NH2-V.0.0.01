48c6b0571cf794f350ce260cb64651c61dcb0f7a
Aryan-Shams <areyancse@gmail.com>
2016-07-15 19:32:29 +0600

    Registration added

--- app/src/main/java/com/shopnosoft/earthquakeresponsesystem/MainActivity.java
+++ app/src/main/java/com/shopnosoft/earthquakeresponsesystem/MainActivity.java
@@ -1,13 +1,179 @@
 package com.shopnosoft.earthquakeresponsesystem;
 
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private WifiManager wfm;
+    private ConnectivityManager cntm;
+    // GPSTracker class
+    GPSTracker gps;
+
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(network() ){
+            createNetErrorDialog();
+        }
+        /*
+//////Checking GPS Status//////////
+        else if(!isLocationServiceEnabled()){
+            gps.showSettingsAlert();
+        }
+        */
+        else
+        {
+            Intent intent = new Intent(this, RegisterActivity_00.class);
+            startActivity(intent);
+        }
+/*
+        else{
+            if(authenticate() == false){
+
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+
+*/
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        if(network()){
+            createNetErrorDialog();
+        }
+        /*
+        else if(!isLocationServiceEnabled()){
+            gps.showSettingsAlert();
+        }
+        */
+        //////Checking GPS Status//////////
+        {
+            Intent intent = new Intent(this, RegisterActivity_00.class);
+            startActivity(intent);
+            finish();
+        }
+        /*
+        else{
+            if(authenticate() == false){
+
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+
+*/
+
+    }
+
+
+
+
+    ////////<<-------------------------------CHECKING NETWORK CONNECTION STATUS------------------------->>///////////////////////
+
+
+    public boolean network() {
+
+//Get the wifi service of device
+        wfm = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+//Get the Mobile Dtata service of device
+        cntm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        final NetworkInfo nInfo = cntm.getActiveNetworkInfo();
+
+        if ((wfm.isWifiEnabled()) || (nInfo != null && nInfo.isConnected())) {
+            return true;
+        }
+
+        else{
+            return false;
+        }
+
+    }
+
+
+////////<<-------------------------------CHECKING NETWORK CONNECTION STATUS ENDS-------------------->>///////////////////////
+
+////////<<-------------------------------CHECKING GPS  STATUS Starts-------------------->>///////////////////////
+
+public boolean isLocationServiceEnabled(){
+
+        LocationManager locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            //GPS enabled
+            return true;
+        }
+
+        else{
+            //GPS disabled
+            return false;
+        }
+    }
+
+////////<<-------------------------------CHECKING GPS  STATUS Ends-------------------->>///////////////////////
+
+
+///////<<-------------------------------CREATING ERROR DIALOG--------------------------------------->>////////////////////////
+
+    protected void createNetErrorDialog() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You need a network connection to use this application. Please turn on mobile network or Wi-Fi in Settings.")
+                .setTitle("Unable to connect")
+                .setCancelable(false)
+                .setPositiveButton("Enable",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent i = new Intent(Settings.ACTION_SETTINGS);
+
+                                //startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+
+                                startActivity(i);
+                                //finish();
+                            }
+                        }
+                )  .setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                }
+        )
+
+        ;
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+///////<<-------------------------------CREATING ERROR DIALOG ENDS--------------------------------------->>///////////////////
+
+
 }
---
+++ app/src/main/java/com/shopnosoft/earthquakeresponsesystem/RegisterActivity_00.java
@@ -0,0 +1,119 @@
+package com.shopnosoft.earthquakeresponsesystem;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.ResponseCache;
+
+public class RegisterActivity_00 extends AppCompatActivity {
+    //for Double back press
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_activity_00);
+
+
+//Initializing the veriables
+
+        final EditText etfullnamereg = (EditText) findViewById(R.id.etnamereg);
+        final EditText etmailreg = (EditText) findViewById(R.id.etemailreg);
+        final EditText etphonenoreg = (EditText) findViewById(R.id.etphnnoreg);
+        final EditText etusernamereg = (EditText) findViewById(R.id.etusrnamereg);
+        final EditText etpasswordreg = (EditText) findViewById(R.id.etpasswordreg);
+        final EditText etrepasswordreg = (EditText) findViewById(R.id.etrepasswordreg);
+
+
+        final Button bRegisterinreg = (Button) findViewById(R.id.bRegisterreg);
+
+        bRegisterinreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+//Getting Data from the fields
+
+                final String name=etfullnamereg.getText().toString();
+                final String email=etmailreg.getText().toString();
+                final String mobileno=etphonenoreg.getText().toString();
+                final String username=etusernamereg.getText().toString();
+                final String password=etpasswordreg.getText().toString();
+
+                Response.Listener<String> responseListner = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+
+////////////////////////Storing USer Data Locally/////////////////////////////////////////////
+                           /*     User registeredData = new User (name,email,username,password,mobileno);
+                                userLocalStore.storeUserData(registeredData);
+                                userLocalStore.setUserLoggedIn(true);
+                                */
+//////////////////////Storing USer Data Locally/ ends////////////////////////////////////
+
+                                /*
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+
+                                */
+                                finish();
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity_00.this);
+                                builder.setMessage("User Already Exists!!! Resistration  Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                RegisterRequest_001 registerRequest = new RegisterRequest_001 (name,email,username,password,mobileno,responseListner );
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity_00.this);
+                queue.add(registerRequest);
+
+
+
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(getBaseContext(), "Press once again to exit!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
+    }
+
+
+}
